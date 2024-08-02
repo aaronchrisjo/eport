33f8d28463d2afc19fa22e91ba1fb03623545380
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-02 18:45:52 +0530

    Dark mode started working now . Implemented for navbar and home component

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private darkModeService: ThemeService) {}
 }
