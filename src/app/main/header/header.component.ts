420f1f79428bfd48848b2a9dec5e88050e59d1a8
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-02 13:05:05 +0530

    Added a skills component with a set of skills. Added icon package for almost all technologies.

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isContactModalVisible = false;
+  isMenuOpen = false; 
 
   openContactModal() {
     this.isContactModalVisible = true;
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -8,6 +8,7 @@ import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { FooterComponent } from './footer/footer.component';
     AboutComponent,
     ResumeComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    SkillsComponent
   ],
   imports: [
     CommonModule,
@@ -29,7 +31,8 @@ import { FooterComponent } from './footer/footer.component';
     AboutComponent,
     ResumeComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    SkillsComponent
   ]
 })
 export class MainModule { }
